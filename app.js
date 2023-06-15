c8a972740d40b870d278135e7c9fe07918b0c607
KC-426 <kuldeepchahar426@gmail.com>
2023-06-15 15:32:28 +0530

    update

--- app.js
+++ app.js
@@ -14,13 +14,16 @@ console.log('-------------server started------------------------')
 
 console.log('-------------server started------------------------')
 
+console.log('-------------server started------------------------')
+
+console.log('-------------server started------------------------')
 
 app.use('/auth', authRoutes);
 app.use('/blog', blogRoutes)
 
 mongoose.connect('mongodb+srv://kuldeep:18330468@cluster0.qw8m0tp.mongodb.net/')
 .then(result => {
-    app.listen(3000)
+    app.listen(5000)
 })
 .catch(err => {
     console.log(err)
